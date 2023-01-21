94ec82b0665379761f760baaa7c16a58894485eb
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-21 04:11:31 +0100

    announce implemented, zombie class file added

---
+++ ex00/Zombie.class.cpp
@@ -0,0 +1,18 @@
+#include "Zombie.class.hpp"
+
+void Zombie::announce(void)
+{
+	std::cout 
+		<< this->name << "BraiiiiiiinnnzzzZ..."
+	<< std::endl;
+}
+
+Zombie::Zombie(void)
+{
+	return ;
+}
+
+Zombie::~Zombie(void)
+{
+	return ;
+}
