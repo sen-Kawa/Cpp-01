f0b7f33e40314db95c132ecbc8b2159ec2a2d83d
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-21 05:59:40 +0100

    printing memory addresses

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,21 @@
 #include <string>
+#include <iostream>
 
 int	main(void)
 {
 	std::string str = "HI THIS IS BRAIN.";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+	
+	std::cout
+		<< "Memory address of string variable: "
+		<< &str << std::endl
+		<< "Memory address held by stringPTR: "
+		<< &stringPTR << std::endl
+		<< "Memory address held by stringREF: "
+		<< &stringREF << std::endl
+	<< std::endl;
+	return (0);
+
 
 }
