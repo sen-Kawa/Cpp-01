8705f05c684ab23d0ed0e912aca13a55377c5840
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-21 03:37:50 +0100

    added file zombie class

---
+++ ex00/Zombie.class.hpp
@@ -0,0 +1,18 @@
+#ifndef ZOMBIE_CLASS_HPP
+# define ZOMBIE_CLASS_HPP
+
+#include <string>
+
+class Zombie
+{
+	private:
+
+		std::string	name;
+
+	public:
+		
+		Zombie(void);
+		~Zombie(void);
+};
+
+#endif
