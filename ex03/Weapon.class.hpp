0032e21109109b6800726d28c32cf5703874fd75
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-22 04:23:49 +0100

    starting weapon class with type attribute

---
+++ ex03/Weapon.class.hpp
@@ -0,0 +1,18 @@
+#ifndef WEAPON_CLASS_HPP
+# define WEAPON_CLASS_HPP
+
+#include <string>
+
+class Weapon
+{
+		private:
+
+			std::string type;
+
+		public:
+
+			Weapon(void);
+			~Weapon(void);
+};
+
+#endif
