d610e6c3b1715db98897b51f6306488f0916ce8e
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-23 20:52:35 +0100

    human A attack

--- ex03/HumanA.class.cpp
+++ ex03/HumanA.class.cpp
@@ -1,7 +1,17 @@
 #include "Weapon.class.hpp"
 #include "HumanA.class.hpp"
+#include <iostream>
+#include <string>
 
-HumanA::HumanA(void)
+void	HumanA::attack(void)
+{
+	std::cout
+		<< name << "attacks with their" << weapon.getType()
+	<< std::endl;
+	return ;
+}
+
+HumanA::HumanA(std::string name, Weapon weapon) : name(name), weapon(weapon)
 {
 	return ;
 }
