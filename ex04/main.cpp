95c025f63fdd55c6d030830da8fbdef145cf13ec
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-23 21:56:08 +0100

    arg checker when opening filestream

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <sstream>
 
-int	main(int argc, char **argv)
+int	arg_check(int argc, char **argv, std::ifstream &ifs)
 {
-	(void) argv;
 	if (argc != 4)
 	{
 		std::cout 
-			<< "Invalid input" << std::endl;
-		return (1);
+			<< "Invalid input, 4 args needed."
+		<< std::endl;
+		return (-1);
+	}
+	if (!ifs.good())
+	{
+		std::cout 
+			<< "Error when reading from file " << argv[1]
+		<< std::endl;
+		return (-1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+		(void)argv;
+	std::ifstream	ifs(argv[1]);
+
+	if (arg_check(argc, argv, ifs) == -1)
+	{
+		return (-1);
 	}
 	return (0);
 }
