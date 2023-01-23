e0a4f30aa15d088f57614dcef50bf5ccaf4c25bd
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-23 23:06:11 +0100

    adding test file

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,9 +1,10 @@
+#include <cstddef>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 
-int	arg_check(int argc, char **argv, std::ifstream &ifs)
+int	arg_check(int argc, char **argv, std::ifstream &ifs, std::ofstream &ofs)
 {
 	if (argc != 4)
 	{
@@ -19,17 +20,51 @@ int	arg_check(int argc, char **argv, std::ifstream &ifs)
 		<< std::endl;
 		return (-1);
 	}
+	if (!ofs.good())
+	{
+		std::cout 
+			<< "Error when creating output file."
+		<< std::endl;
+		return (-1);
+	}
 	return (0);
+}
+
+void	replace(std::ifstream &ifs, std::ofstream &ofs, std::string s1, std::string s2)
+{
+	std::string		line;
+	size_t			position;
+
+	if (s1.length() == 0)
+	{
+		std::cout
+			<< "Enter a valid string for S1"
+		<< std::endl;
+		return ;
+	}
+	while (std::getline(ifs, line))
+	{
+		while ((position = line.find(s1)) != std::string::npos)
+		{
+			line.erase(position, s1.length());
+			line.insert(position, s2);
+		}
+		ofs << line << std::endl;
+	}
+	return ;
 }
 
 int	main(int argc, char **argv)
 {
-		(void)argv;
 	std::ifstream	ifs(argv[1]);
+	std::ofstream	ofs("replace.txt");
 
-	if (arg_check(argc, argv, ifs) == -1)
+	if (arg_check(argc, argv, ifs, ofs) == -1)
 	{
 		return (-1);
 	}
+	replace(ifs, ofs, argv[2], argv[3]);
+	ifs.close();
+	ofs.close();
 	return (0);
 }
