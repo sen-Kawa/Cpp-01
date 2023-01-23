e666e5335f419fc74a1bf83db10193f1ed35feaa
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-24 00:45:41 +0100

    main calling complain with different levels

---
+++ ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.class.hpp"
+
+int	main(void)
+{
+	Harl harl;
+
+	harl.complain("ANDERE");
+	harl.complain("WARNING");
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("ERROR");
+	return (0);
+}
