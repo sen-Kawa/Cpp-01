a1e8842fb7cacafba5f2880cda6d3a1f10c0918a
olin_tp_ar <universalexperimental@protonmail.com>
2023-01-24 01:09:28 +0100

    main taking input

---
+++ ex06/main.cpp
@@ -0,0 +1,16 @@
+#include "Harl.class.hpp"
+
+int	main(int argc, char **argv)
+{
+	Harl harl;
+
+	if (argc != 2)
+	{
+		std::cout
+			<< "Invalid amount of arguments."
+		<< std::endl;
+		return (-1);
+	}
+	harl.complain(argv[1]);
+	return (0);
+}
